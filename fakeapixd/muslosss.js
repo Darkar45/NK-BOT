3677cb4f85adc6999e085b2a82910c7238d097ea
Darkar45 <89761748+Darkar45@users.noreply.github.com>
2021-08-30 00:23:58 -0500

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -1,4 +1,4 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvMRPeHtlg3wipWxCS055lBMcQpYn2Pogdyw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwjGF4urnf7AI5zVNIaZvbGHtt3u--CzBDw&usqp=CAU"}
-]
+{"result": "https://rule34.xxx/index.php?page=post&s=list&tags=panties"} 
